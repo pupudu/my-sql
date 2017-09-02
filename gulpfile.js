003ac8e48fd9db7affb652cafbe0dcd3cee44b85
Pubudu Dodangoda <pubudu.dodangoda@trycake.com>
2017-09-02 13:54:30 +0530

    Add 3rd party code quality tool config files and build files

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+
+gulp.task('babel', () =>
+    gulp.src(['src/**/*.js', '!src/**/*.test.js'])
+        .pipe(babel())
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('default', ['babel']);
